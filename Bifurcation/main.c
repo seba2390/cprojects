c6487e70d0c74d1d1b7fb8dc6c7194bc0b649108
seba2390 <madsen3008@gmail.com>
2021-02-26 23:14:36 +0100

    bifurcation updated

--- Bifurcation/main.c
+++ Bifurcation/main.c
@@ -1,36 +1,68 @@
 #include<stdio.h>
+#include<omp.h>
 
 double population_func(double x_n, double r)
 {
         double growth_rate = r;
-        return growth_rate*x_n*(1.0-x_n);
+        return growth_rate*x_n*((double)1-x_n);
 }
 
 
 int main()
 {
-	
-	double init_pop_size = 0.5;
-	double growth_rate = 0.0;
+	double init_pop_size = 0.300;
+	int nr_of_threads = 3;
 	double max_growth_rate = 4.0;
 
-	double nr_steps = 1000000;
-	double step_size = max_growth_rate/nr_steps;
-	double tolerance = 1.0e-13;
-	
-	for(int j = 1; j <= nr_steps; j++)
+	int nr_steps = (int)1e6;
+	double step_size = max_growth_rate/((double)(nr_of_threads*nr_steps));
+	#pragma omp parallel sections
 	{
-		double pop_size = init_pop_size;
-		for(int i = 1; i <= 1000; i++)
+		#pragma omp section
 		{
-		pop_size = population_func(pop_size,growth_rate);
-		if(pop_size<tolerance)
-			{
-				pop_size = 0.0;
-			}
+			double growth_rate = 0.0;
+			for(int j = 1; j <= nr_steps; j++)
+				{
+				double pop_size = init_pop_size;
+				for(int i = 1; i <= (int)1e3; i++)
+					{
+					pop_size = population_func(pop_size,growth_rate);
+					}
+				printf("%10g %10g \n",growth_rate,pop_size);
+				growth_rate += step_size;		
+				}
 		}
-		printf("%10g %10g \n",growth_rate,pop_size);
-		growth_rate += step_size;		
+		#pragma omp section
+		{
+			double growth_rate = 0.0;
+			growth_rate += nr_steps*step_size;
+			for(int j = 1; j <= nr_steps; j++)
+				{
+				double pop_size = init_pop_size;
+				for(int i = 1; i <= (int)1e3; i++)
+					{
+					pop_size = population_func(pop_size,growth_rate);
+					}
+				printf("%10g %10g \n",growth_rate,pop_size);
+				growth_rate += step_size;		
+				}
+		}
+		#pragma omp section
+		{
+			double growth_rate = 0.0;
+			growth_rate += 2.0*nr_steps*step_size;
+			for(int j = 1; j <= nr_steps; j++)
+				{
+				double pop_size = init_pop_size;
+				for(int i = 1; i <= (int)1e3; i++)
+					{
+					pop_size = population_func(pop_size,growth_rate);
+					}
+				printf("%10g %10g \n",growth_rate,pop_size);
+				growth_rate += step_size;		
+				}
+		}
+		
 	}
 
 }
